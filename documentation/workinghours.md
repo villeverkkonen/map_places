b65df2ae69175da9f71fb6a6a3ac682912f01560
Ville Verkkonen <ville@pulmaton.fi>
2019-04-12 22:32:46 +0300

    Created maven project with Spring Boot

---
+++ documentation/workinghours.md
@@ -0,0 +1,8 @@
+# Working hours monitoring
+
+## Total
+= X hours
+
+| Date  | Time | Work done |
+|-------|------|-----------|
+| 12.4. |      | Getting started |
---
+++ src/main/java/map_places/MapPlacesApplication.java
@@ -0,0 +1,10 @@
+package map_places;
+
+import org.springframework.boot.SpringApplication;
+
+public class MapPlacesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapPlacesApplication.class, args);
+    }
+}
